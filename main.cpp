c72c2bf98657882c46afe55cd2bbfb69bdf967cb
abhishtagatya <abhishtagatya@yahoo.com>
2024-05-21 15:54:30 +0200

    Added Model Loading

--- main.cpp
+++ main.cpp
@@ -2,52 +2,14 @@
 // Defined before OpenGL and GLUT includes to avoid deprecation messages
 #define GL_SILENCE_DEPRECATION
 #include "mesh.h"
+#include "model.h"
 #endif
 
 #include <iostream>
 
-// Vertices coordinates
-Vertex vertices[] = { //     COORDINATES     /        COLORS        /    TexCoord    /       NORMALS     //
-        Vertex{glm::vec3(-1.0f, 0.0f,  1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.0f, 0.0f)},
-        Vertex{glm::vec3(-1.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(0.0f, 1.0f)},
-        Vertex{glm::vec3( 1.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(1.0f, 1.0f)},
-        Vertex{glm::vec3( 1.0f, 0.0f,  1.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), glm::vec2(1.0f, 0.0f)}
-};
-
-// Indices for vertices order
-GLuint indices[] = {
-        0, 1, 2,
-        0, 2, 3
-};
-
-Vertex lightVertices[] = { //     COORDINATES     //
-        Vertex{glm::vec3(-0.1f, -0.1f,  0.1f)},
-        Vertex{glm::vec3(-0.1f, -0.1f, -0.1f)},
-        Vertex{glm::vec3(0.1f, -0.1f, -0.1f)},
-        Vertex{glm::vec3(0.1f, -0.1f,  0.1f)},
-        Vertex{glm::vec3(-0.1f,  0.1f,  0.1f)},
-        Vertex{glm::vec3(-0.1f,  0.1f, -0.1f)},
-        Vertex{glm::vec3(0.1f,  0.1f, -0.1f)},
-        Vertex{glm::vec3(0.1f,  0.1f,  0.1f)}
-};
-
-GLuint lightIndices[] = {
-        0, 1, 2,
-        0, 2, 3,
-        0, 4, 7,
-        0, 7, 3,
-        3, 7, 6,
-        3, 6, 2,
-        2, 6, 5,
-        2, 5, 1,
-        1, 5, 4,
-        1, 4, 0,
-        4, 5, 6,
-        4, 6, 7
-};
-
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 800;
+
 int main()
 {
 
@@ -72,47 +34,25 @@ int main()
 
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
-    Texture textures[]{
-            Texture("resources/textures/planks.png", "diffuse", 0, GL_RGBA, GL_UNSIGNED_BYTE),
-            Texture("resources/textures/planksSpec.png", "specular", 1, GL_RED, GL_UNSIGNED_BYTE)
-    };
-
     Shader shaderProgram(
-            "resources/shaders/mesh.vert",
-            "resources/shaders/mesh.frag"
+            "resources/shaders/model.vert",
+            "resources/shaders/model.frag"
     );
-    std::vector <Vertex> verts(vertices, vertices + sizeof(vertices) / sizeof(Vertex));
-    std::vector <GLuint> ind(indices, indices + sizeof(indices) / sizeof(GLuint));
-    std::vector <Texture> tex(textures, textures + sizeof(textures) / sizeof(Texture));
-    Mesh floor(verts, ind, tex);
-
-    Shader lightShader("resources/shaders/light.vert",
-                       "resources/shaders/light.frag");
-    std::vector <Vertex> lightVerts(lightVertices, lightVertices + sizeof(lightVertices) / sizeof(Vertex));
-    std::vector <GLuint> lightInd(lightIndices, lightIndices + sizeof(lightIndices) / sizeof(GLuint));
-    Mesh light(lightVerts, lightInd, tex);
 
     glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
     glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
     glm::mat4 lightModel = glm::mat4(1.0f);
     lightModel = glm::translate(lightModel, lightPos);
 
-    glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::mat4 pyramidModel = glm::mat4(1.0f);
-    pyramidModel = glm::translate(pyramidModel, pyramidPos);
-
-    lightShader.Activate();
-    glUniformMatrix4fv(glGetUniformLocation(lightShader.id, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
-    glUniform4f(glGetUniformLocation(lightShader.id, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-
     shaderProgram.Activate();
-    glUniformMatrix4fv(glGetUniformLocation(shaderProgram.id, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
     glUniform4f(glGetUniformLocation(shaderProgram.id, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
     glUniform3f(glGetUniformLocation(shaderProgram.id, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
     Camera camera(SCR_WIDTH, SCR_HEIGHT, glm::vec3(0.0f, 0.0f, 2.0f));
 
     glEnable(GL_DEPTH_TEST);
+
+    Model model("resources/model/map/scene.gltf");
 
     // Keeping the Window Alive
     while (!glfwWindowShouldClose(window)) {
@@ -122,8 +62,7 @@ int main()
         camera.Inputs(window);
         camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
-        floor.Draw(shaderProgram, camera);
-        light.Draw(lightShader, camera);
+        model.Draw(shaderProgram, camera);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -131,7 +70,6 @@ int main()
     }
 
     shaderProgram.Delete();
-    lightShader.Delete();
 
     // Destroy the Window
     glfwDestroyWindow(window);
